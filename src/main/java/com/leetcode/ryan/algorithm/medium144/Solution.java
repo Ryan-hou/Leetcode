2e8e985a5f38e4bf207789dee8a5c9d5fd1310f9
Ryan Hou <qq676476208@gmail.com>
2017-07-18 19:21:36 +0800

    144 Binary Tree Preorder Traversal is finished.

---
+++ src/main/java/com/leetcode/ryan/algorithm/medium144/Solution.java
@@ -0,0 +1,82 @@
+package com.leetcode.ryan.algorithm.medium144;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author yanlin.hou@ucarinc.com
+ * @description:
+ * @className: Solution
+ * @date July 18,2017
+ */
+@Slf4j
+public class Solution {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * Recursive solution
+     * @param root
+     * @return
+     */
+    private static List<Integer> preorderTraversalOne(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        doPreOrderTraversal(root, result);
+        return result;
+    }
+
+    private static void doPreOrderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) return;
+        result.add(root.val);
+        doPreOrderTraversal(root.left, result);
+        doPreOrderTraversal(root.right, result);
+    }
+
+    /**
+     * Iterative solution
+     * @param root
+     * @return
+     */
+    private static List<Integer> preorderTraversalTwo(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                result.add(root.val);
+                if (root.right != null) {
+                    stack.push(root.right);
+                }
+                root = root.left;
+            } else {
+                root = stack.pop();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode left1 = new TreeNode(4);
+        TreeNode left12 = new TreeNode(6);
+        TreeNode right1 = new TreeNode(5);
+        TreeNode rigth12 = new TreeNode(7);
+        root.left = left1;
+        left1.right = left12;
+        root.right = right1;
+        right1.left = rigth12;
+        List<Integer> integers = preorderTraversalTwo(root);
+        log.info("result = {}", integers);
+    }
+}
---
+++ src/main/java/com/leetcode/ryan/algorithm/medium144/readme.md
@@ -0,0 +1,5 @@
+### 144. Binary Tree Preorder Traversal
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Note: Recursive solution is trivial, could you do it iteratively?
