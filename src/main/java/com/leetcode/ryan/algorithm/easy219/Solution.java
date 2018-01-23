cca02e54e18c97c4485dcf52b717bf8296e72d2d
ryan_hou <ryan.houyl@gmail.com>
2018-01-23 10:44:12 +0800

    219 is finished.

---
+++ src/main/java/com/leetcode/ryan/algorithm/easy219/Solution.java
@@ -0,0 +1,56 @@
+package com.leetcode.ryan.algorithm.easy219;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description:
+ * @className: Solution
+ * @date January 23,2018
+ */
+@Slf4j
+public class Solution {
+
+    /**
+     * 思路一：暴力解法，找出所有的i,j 使得 nums[i] == nums[j], |i-j| <= k
+     * 时间复杂度O(n^2)
+     */
+
+    /**
+     * 思路二：滑动窗口+查找表
+     * 构造长度为k+1的滑动窗口，每次在移动窗口时，比较下一个要遍历的元素是否在滑动窗口所构成的查找表中
+     */
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+
+        Set<Integer> record = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (record.contains(nums[i])) {
+                return true;
+            }
+
+            record.add(nums[i]);
+
+            //保持record中最多有k个元素
+            if (record.size() == k + 1) {
+                record.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 5, 6, 7, 2, 9};
+        int k = 5;
+        boolean res = containsNearbyDuplicate(nums, k);
+        log.info("result = {}", res);
+    }
+}
