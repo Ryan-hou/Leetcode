3a2c798c036ec45aa230bbb8f7c2da981d418a79
ryan_hou <ryan.houyl@gmail.com>
2018-01-29 20:23:15 +0800

    279th is finished.

---
+++ src/main/java/com/leetcode/ryan/algorithm/medium/medium279/Solution.java
@@ -0,0 +1,10 @@
+package com.leetcode.ryan.algorithm.medium.medium279;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description:
+ * @className: Solution
+ * @date January 29,2018
+ */
+public class Solution {
+}
