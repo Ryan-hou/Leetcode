cc4ddb4715711c22c2bf20a0c2a3cd904c75736c
ryan_hou <ryan.houyl@gmail.com>
2018-02-06 11:47:27 +0800

    416th is finished.

---
+++ src/main/java/com/leetcode/ryan/algorithm/medium/medium416/Solution.java
@@ -0,0 +1,10 @@
+package com.leetcode.ryan.algorithm.medium.medium416;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description:
+ * @className: Solution
+ * @date February 06,2018
+ */
+public class Solution {
+}
