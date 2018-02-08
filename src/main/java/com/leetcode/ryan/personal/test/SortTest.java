4f665637b08fa5fa27909d42e1d7d886faad732b
ryan_hou <ryan.houyl@gmail.com>
2018-02-08 11:21:41 +0800

    quick sort is finished.

--- src/main/java/com/leetcode/ryan/personal/test/SortTest.java
+++ src/main/java/com/leetcode/ryan/personal/test/SortTest.java
@@ -1,6 +1,7 @@
 package com.leetcode.ryan.personal.test;
 
 import com.leetcode.ryan.personal.sort.MergeSort;
+import com.leetcode.ryan.personal.sort.QuickSort;
 import com.leetcode.ryan.personal.sort.Sort;
 import com.leetcode.ryan.personal.util.ArrayUtil;
 
@@ -13,10 +14,13 @@ import com.leetcode.ryan.personal.util.ArrayUtil;
 public class SortTest {
 
     public static void main(String[] args) {
-        int n = 1000000;
-        int[] arr = ArrayUtil.generateRandomArray(n, 0, n);
+        int n = 500000;
+        int[] arr = ArrayUtil.generateRandomArray(n, 0, 10);
+        //int[] arr = ArrayUtil.generateNearlyOrderedArray(n, 10);
+
         int[] copyArray = ArrayUtil.copyArray(arr);
         testSort("Merge sort:", arr, n, new MergeSort());
+        testSort("Quick sort:", copyArray, n, new QuickSort());
         //testSort("Selection sort:", arr, n, new SelectionSort());
         //testSort("Insert sort:", copyArray, n, new InsertSort());
     }
