67ceb1bb78df1289a0821e62c13f3459fe145432
ryan_hou <ryan.houyl@gmail.com>
2018-08-16 16:04:30 +0800

    ds_redblack_tree finished.

--- src/main/java/com/github/ryan/personal/data_structure/deque/Deque.java
+++ src/main/java/com/github/ryan/personal/data_structure/deque/Deque.java
@@ -64,7 +64,167 @@ package com.github.ryan.personal.data_structure.deque;
  * from the beginning of the deque. Stack methods are precisely equivalent
  * to {@code Deque} methods as following:
  *
+ * {@link #push(Object)}
+ * {@link Deque#addFirst(Object)}
+ *
+ * {@link #pop()}
+ * {@link Deque#removeFirst()}
+ *
+ * {@link #peek()}
+ * {@link Deque#peekFirst()}
+ *
+ * Note that the {@link #peek()} method works equally well when
+ * a deque is used as a queue or a stack; in either case, elements
+ * are drawn from the beginning of the deque.
+ *
+ * Unlink the List interface, this interface does not
+ * provide support for indexed access to elements.
+ *
+ * @param <E> the type of elements held in this collection
  */
 public interface Deque<E> extends Queue<E> {
+
+
+    /**
+     * Inserts the specified element at the front of this deque if it is
+     * possible to do so immediately without violating capacity restrictions,
+     * throwing an @{code IllegalStateException} if no space is currently
+     * available. When using a capacity-restricted deque, it is generally
+     * preferable to use method {@link #offerFirst}.
+     *
+     * @param e the element to add
+     * @throws IllegalStateException if the element cannot be added at this
+     *          time due to capacity restrictions
+     * @throws ClassCastException if the class of the specified element
+     *          prevents it from being added to this deque.
+     * @throws NullPointerException if the specified element is null and
+     *          this deque does not permit null elements
+     * @throws IllegalArgumentException if some property of the specified
+     *          element prevents it from being added to this deque
+     */
+    void addFirst(E e);
+
+    /**
+     * @param e the element to add
+     * @return {@code true} if the element was added to this deque, else
+     *      {@code false}
+     */
+    boolean offerFirst(E e);
+
+    void addLast(E e);
+
+    boolean offerLast(E e);
+
+    /**
+     * Retrieves and removes the first element of this deque. This method
+     * differs from {@link #pollFirst} only in that it throws an exception
+     * if this deque is empty.
+     *
+     * @return the head of this deque
+     * @throws java.util.NoSuchElementException if this deque is empty
+     */
+    E removeFirst();
+
+    E pollFirst();
+
+    E removeLast();
+
+    E pollLast();
+
+    /**
+     * Retrieves, but does not remove, the first element of this deque.
+     *
+     * This method differs from {@link #peekFirst} only in that throws
+     * an exception if this deque if empty.
+     *
+     * @return the head of this deque
+     * @throws java.util.NoSuchElementException if this deque is empty
+     */
+    E getFirst();
+
+    E peekFirst();
+
+    E getLast();
+
+    E peekLast();
+
+
+    // *** Queue methods ***
+
+    /**
+     * Equivalent to {@link #addLast(Object)}
+     *
+     * @param e the element to add
+     */
+    boolean add(E e);
+
+    /**
+     * Equivalent to {@link #offerLast(Object)}
+     *
+     * @param e the element to add
+     */
+    boolean offer(E e);
+
+    /**
+     * Equivalent to {@link #removeFirst()}
+     */
+    E remove();
+
+    /**
+     *  Equivalent to {@link #pollFirst()}
+     */
+    E poll();
+
+    /**
+     * Equivalent to {@link #getFirst()}
+     */
+    E element();
+
+    /**
+     * Retrieves, but does not remove, the head of the queue represented by
+     * this deque(in other words, the first element of this deque), or
+     * returns {@code null} if this deque is empty.
+     *
+     * This method is equivalent to {@link #peekFirst()}
+     *
+     * @return the head of the deque represented by this deque, or
+     *      {@code null} if this deque is empty
+     */
+    E peek();
+
+    // *** Stack methods ***
+
+    /**
+     * Pushes an element onto the stack represented by the deque(in other
+     * words, at the head of this deque) if it is possible to do so
+     * immediately without violating capacity restrictions, throwing an
+     * {@code IllegalStateException} if no space is currently available.
+     *
+     * This method is equivalent to {@link #addFirst(Object)}
+     *
+     * @param e the element to push
+     * @throws IllegalStateException if the element cannot be added at this
+     *      time due to capacity restrictions
+     * @throws NullPointerException if the specified element is null and this
+     *      deque does not permit null elements
+     */
+    void push(E e);
+
+
+    /**
+     * Pops an element from the stack represented by this deque. In other
+     * words, removes and returns the first element of this deque.
+     *
+     * This method is equivalent to {@link #removeFirst()}
+     *
+     * @return the element at the front of this deque (which is the top)
+     *          of the stack represented by this deque)
+     * @throws java.util.NoSuchElementException if this deque is empty
+     */
+    E pop();
+
+    // Equivalent to peekFirst()
+    // E peek();
+
 
 }
