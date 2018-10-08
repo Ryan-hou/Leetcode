8a63f5b30ff287672fcf3d20ece11aa298768a8c
ryan_hou <ryan.houyl@gmail.com>
2018-10-08 23:44:04 +0800

    剑指offer算法题-树相关算法

---
+++ src/main/java/com/github/ryan/jianzhi/algorithm/TreeCategory.java
@@ -0,0 +1,29 @@
+package com.github.ryan.jianzhi.algorithm;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className TreeCategory
+ * @date October 08,2018
+ */
+public class TreeCategory {
+
+    private static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 输入一棵二叉树，求该树的深度。
+     * 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+     */
+    public int TreeDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
+    }
+}
---
+++ src/main/java/com/github/ryan/jianzhi/algorithm/readme.md
@@ -0,0 +1,3 @@
+剑指offer的算法题总结
+
+https://www.nowcoder.com/ta/coding-interviews?page=2
