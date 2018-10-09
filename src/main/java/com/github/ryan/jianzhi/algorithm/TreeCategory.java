26a213d343338ae7ba426bb58bf98b332475f370
ryan_hou <ryan.houyl@gmail.com>
2018-10-09 10:17:50 +0800

    剑指offer算法题-树相关算法

--- src/main/java/com/github/ryan/jianzhi/algorithm/TreeCategory.java
+++ src/main/java/com/github/ryan/jianzhi/algorithm/TreeCategory.java
@@ -26,4 +26,33 @@ public class TreeCategory {
         if (root == null) return 0;
         return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
     }
+
+    /**
+     * 操作给定的二叉树，将其变换为源二叉树的镜像。
+     输入描述:
+     二叉树的镜像定义：源二叉树
+        8
+        /  \
+        6   10
+        / \  / \
+        5 7 9 11
+     镜像二叉树
+        8
+        /  \
+       10  6
+       / \  / \
+      11 9 7  5
+     */
+    public void Mirror(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+
+
+
 }
