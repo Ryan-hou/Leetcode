eed86ed864b66fa6de1bd05b5626a69b97baa94b
ryan_hou <ryan.houyl@gmail.com>
2018-10-19 13:42:24 +0800

    剑指offer算法题-算法设计

--- src/main/java/com/github/ryan/jianzhi/algorithm/AlgoDesignCategory.java
+++ src/main/java/com/github/ryan/jianzhi/algorithm/AlgoDesignCategory.java
@@ -3,6 +3,7 @@ package com.github.ryan.jianzhi.algorithm;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
@@ -59,4 +60,41 @@ public class AlgoDesignCategory {
         }
         return;
     }
+
+    // 使用堆维护动态有序数据
+    /**
+     * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，
+     * 那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，
+     * 那么中位数就是所有数值排序之后中间两个数的平均值。我们使用Insert()方法读取数据流，
+     * 使用GetMedian()方法获取当前读取数据的中位数。
+     */
+    // 使用小根堆存储数据流数据大的一半
+    // 使用大根堆存储数据流数据小的一半
+    // 当总数为奇数时，从小根堆顶部取中间值
+    // 求中间值时，只需要取出堆顶的数据即可
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 默认小根堆
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> -o1.compareTo(o2));
+    private int count = 0; // 元素个数
+
+    public void Insert(Integer num) {
+        if (count % 2 == 0) {
+            maxHeap.add(num);
+            int max = maxHeap.poll();
+            minHeap.add(max);
+        } else {
+            minHeap.add(num);
+            int min = minHeap.poll();
+            maxHeap.add(min);
+        }
+        count++;
+    }
+
+    public Double GetMedian() {
+        if (count == 0) return 0.0d;
+
+        return count % 2 == 0
+                ? (minHeap.peek() + maxHeap.peek()) / 2.0
+                : minHeap.peek().doubleValue();
+    }
+
 }
