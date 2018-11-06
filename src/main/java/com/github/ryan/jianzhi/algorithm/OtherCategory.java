7251486cd561d6366647962b95386a9faeb72527
ryan_hou <ryan.houyl@gmail.com>
2018-11-06 20:03:15 +0800

    剑指offer算法题-其他算法

--- src/main/java/com/github/ryan/jianzhi/algorithm/OtherCategory.java
+++ src/main/java/com/github/ryan/jianzhi/algorithm/OtherCategory.java
@@ -300,4 +300,30 @@ public class OtherCategory {
         return res;
     }
 
+    /**
+     * 把只包含质因子2、3和5的数称作丑数（Ugly Number）。例如6、8都是丑数，但14不是，因为它包含质因子7。
+     * 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
+     */
+    public int GetUglyNumber_Solution(int index) {
+
+        if (index <= 0) return 0;
+
+        // 假设i是丑数，那么2i,3i,5i都是丑数
+        List<Integer> list = new ArrayList<>();
+        list.add(1); // 第一个丑数
+        // 三个下标用于记录丑数的位置(很巧妙的避开了构造三个队列    ·)
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (list.size() < index) {
+            int n2 = 2 * list.get(i2);
+            int n3 = 3 * list.get(i3);
+            int n5 = 5 * list.get(i5);
+            int min = Math.min(n2, Math.min(n3, n5));
+            list.add(min);
+            if (min == n2) i2++;
+            if (min == n3) i3++;
+            if (min == n5) i5++;
+        }
+        return list.get(index - 1);
+    }
+
 }
---
+++ src/main/java/com/github/ryan/personal/data_structure/concurrent/executor/ThreadPoolExecutor.java
@@ -0,0 +1,10 @@
+package com.github.ryan.personal.data_structure.concurrent.executor;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className ThreadPoolExecutor
+ * @date November 05,2018
+ */
+public class ThreadPoolExecutor {
+}
