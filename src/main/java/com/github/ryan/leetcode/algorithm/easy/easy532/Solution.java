66d5ccee7ffc38a74b0c7b39cd52409584743108
ryan_hou <ryan.houyl@gmail.com>
2018-12-01 13:09:23 +0800

    Leetcode easy 532 is completed.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy532/Solution.java
@@ -0,0 +1,10 @@
+package com.github.ryan.leetcode.algorithm.easy.easy532;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date December 01,2018
+ */
+public class Solution {
+}
