4a271968c9fa6f5bb326c50e7a08695838f9fdfa
ryan_hou <ryan.houyl@gmail.com>
2018-12-21 19:28:10 +0800

    Leetcode medium120 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium120/Solution.java
@@ -0,0 +1,10 @@
+package com.github.ryan.leetcode.algorithm.medium.medium120;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date December 21,2018
+ */
+public class Solution {
+}
