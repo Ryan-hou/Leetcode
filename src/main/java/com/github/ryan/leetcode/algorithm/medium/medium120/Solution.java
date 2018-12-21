d348be62d6f519db8ddefb5ed4287cb85442b0d6
ryan_hou <ryan.houyl@gmail.com>
2018-12-21 19:28:33 +0800

    Leetcode medium120 is finished.

--- src/main/java/com/github/ryan/leetcode/algorithm/medium/medium120/Solution.java
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium120/Solution.java
@@ -1,5 +1,7 @@
 package com.github.ryan.leetcode.algorithm.medium.medium120;
 
+import java.util.List;
+
 /**
  * @author ryan.houyl@gmail.com
  * @description
@@ -7,4 +9,26 @@ package com.github.ryan.leetcode.algorithm.medium.medium120;
  * @date December 21,2018
  */
 public class Solution {
+
+    // 使用动态规划自底向上处理
+    // 时间复杂度O(n^2), 空间复杂度O(n)
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // from bottom to top, use DP
+        if (triangle == null || triangle.size() == 0) return 0;
+
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                int v = triangle.get(i).get(j);
+                dp[j] = v + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
 }
