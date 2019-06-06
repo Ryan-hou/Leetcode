de9b504ecac841f4f572d641a1b33ec77692be37
ryan_hou <ryan.houyl@gmail.com>
2019-06-06 17:13:17 +0800

    medium11 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium11/Solution.java
@@ -0,0 +1,29 @@
+package com.github.ryan.leetcode.algorithm.medium.medium11;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date June 06,2019
+ */
+public class Solution {
+
+    // two pointers:
+    // time complexity: O(n) / space complexity: O()
+    public int maxArea(int[] height) {
+        assert height != null && height.length > 1;
+        int l = 0, r = height.length - 1;
+        int res = Integer.MIN_VALUE;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                res = Math.max(res, height[l] * (r - l));
+                l++;
+            } else {
+                // height[l] >= height[r]
+                res = Math.max(res, height[r] * (r - l));
+                r--;
+            }
+        }
+        return res;
+    }
+}
