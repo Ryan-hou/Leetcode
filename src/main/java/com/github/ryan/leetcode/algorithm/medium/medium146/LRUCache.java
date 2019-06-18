ea7e8ec2d99ea011125713427db3ae9aabc6b4ca
ryan_hou <ryan.houyl@gmail.com>
2019-06-18 15:08:59 +0800

    medium146 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium146/LRUCache.java
@@ -0,0 +1,32 @@
+package com.github.ryan.leetcode.algorithm.medium.medium146;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date June 18,2019
+ */
+public class LRUCache {
+
+    private final Map<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
