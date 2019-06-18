0fd676ef4eb990290839b1fa6c2b1851b61966e0
ryan_hou <ryan.houyl@gmail.com>
2019-06-18 17:38:51 +0800

    medium240 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium240/Solution.java
@@ -0,0 +1,32 @@
+package com.github.ryan.leetcode.algorithm.medium.medium240;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date June 18,2019
+ */
+public class Solution {
+
+    // time complexity: O(m + n)
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // start from top right, move to left down
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                // matrix[row][col] > target
+                col--;
+            }
+        }
+        return false;
+    }
+
+}
