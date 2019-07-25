c82988958cacd4bac78535948d6e24e4ef1fdbf9
ryan_hou <ryan.houyl@gmail.com>
2019-07-25 14:32:21 +0800

    medium129 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium129/Solution.java
@@ -0,0 +1,26 @@
+package com.github.ryan.leetcode.algorithm.medium.medium129;
+
+import com.github.ryan.personal.algorithm.component.TreeNode;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date July 25,2019
+ */
+public class Solution {
+
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0, 0);
+    }
+
+    private int dfs(TreeNode root, int sum, int num) {
+        if (root == null) return 0;
+        num = num * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum + num;
+        }
+        return dfs(root.left, sum, num) + dfs(root.right, sum, num);
+    }
+
+}
