d78a4fa49a2c574ea9c5f18a96bb093ed2790fa1
ryan_hou <ryan.houyl@gmail.com>
2019-08-09 15:47:21 +0800

    medium179 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium179/Solution.java
@@ -0,0 +1,38 @@
+package com.github.ryan.leetcode.algorithm.medium.medium179;
+
+import java.util.Arrays;
+
+/**
+ * @author ryan.houyl@gmail.com
+ * @description
+ * @className Solution
+ * @date August 09,2019
+ */
+public class Solution {
+
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return "";
+        if (checkZero(nums)) return "0";
+
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, (s1, s2) -> (s1 + s2).compareTo(s2 + s1));
+
+        StringBuilder b = new StringBuilder();
+        for (int i = strs.length - 1; i >= 0; i--) {
+            b.append(strs[i]);
+        }
+        return b.toString();
+    }
+
+    private boolean checkZero(int[] nums) {
+        for (int n : nums) {
+            if (n != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
