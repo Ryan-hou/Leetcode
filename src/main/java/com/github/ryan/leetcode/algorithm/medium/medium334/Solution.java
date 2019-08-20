762dfadf813aebea3137b6559f16d07a2447b4eb
ryan_hou <ryan.houyl@gmail.com>
2019-08-20 16:58:34 +0800

    medium334 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium334/Solution.java
@@ -0,0 +1,28 @@
+package com.github.ryan.leetcode.algorithm.medium.medium334;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Solution {
+
+    public boolean increasingTriplet(int[] nums) {
+        if (nums == null) return false;
+        int min = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n < min) {
+                min = n;
+            } else if (n > min && n < mid) {
+                mid = n;
+            } else if (n > mid) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 5, 5, 2, 5, 4};
+        log.info("res = {}", new Solution().increasingTriplet(nums));
+    }
+}
