686e9f6bc572662d6874bb99c17556ce8c578b45
ryan_hou <ryan.houyl@gmail.com>
2019-08-23 10:19:36 +0800

    medium324 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium324/Solution.java
@@ -0,0 +1,23 @@
+package com.github.ryan.leetcode.algorithm.medium.medium324;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    // time complexity: O(nlogn), space complexity: O(n) -> actually beat 100% in leetcode
+    public void wiggleSort(int[] nums) {
+        if (nums == null || nums.length <= 1) return;
+
+        int[] tmp = Arrays.copyOf(nums, nums.length);
+        int idx = tmp.length - 1;
+        Arrays.sort(tmp);
+        for (int i = 1; i < nums.length; i += 2) {
+            nums[i] = tmp[idx--];
+        }
+        for (int i = 0; i < nums.length; i += 2) {
+            nums[i] = tmp[idx--];
+        }
+
+    }
+
+}
