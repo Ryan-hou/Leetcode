f3a80687a19d70cb8b167734a0dffc7c8dc6a143
ryan_hou <ryan.houyl@gmail.com>
2019-08-26 19:33:03 +0800

    medium738 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium738/Solution.java
@@ -0,0 +1,22 @@
+package com.github.ryan.leetcode.algorithm.medium.medium738;
+
+public class Solution {
+
+    public int monotoneIncreasingDigits(int N) {
+        char[] ches = String.valueOf(N).toCharArray();
+        // find the cliff: the first index that ches[i - 1] > ches[i]
+        int i = 1;
+        while (i < ches.length && ches[i - 1] <= ches[i]) {
+            i++;
+        }
+        // move backward for ensusing that ches[i - 1] <= ches[i]
+        while (i > 0 && i < ches.length
+                && ches[i - 1] > ches[i]) {
+            ches[--i]--;
+        }
+        for (int j = i + 1; j < ches.length; j++) {
+            ches[j] = '9';
+        }
+        return Integer.parseInt(String.valueOf(ches));
+    }
+}
