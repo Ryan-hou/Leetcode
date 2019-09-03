70208d0b2e0499e0e9a86cc1946d66a94907daea
ryan_hou <ryan.houyl@gmail.com>
2019-09-03 11:34:04 +0800

    hard899 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/hard/hard899/Solution.java
@@ -0,0 +1,23 @@
+package com.github.ryan.leetcode.algorithm.hard.hard899;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public String orderlyQueue(String S, int K) {
+        if (K > 1) {
+            char[] ches = S.toCharArray();
+            Arrays.sort(ches);
+            return new String(ches);
+        }
+
+        String res = S;
+        for (int i = 1; i < S.length(); i++) {
+            String tmp = S.substring(i) + S.substring(0, i);
+            if (tmp.compareTo(res) < 0) {
+                res = tmp;
+            }
+        }
+        return res;
+    }
+}
