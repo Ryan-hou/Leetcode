29164dd6d2049910515be37d6715e0658ee06bb7
ryan_hou <ryan.houyl@gmail.com>
2019-09-07 23:03:12 +0800

    easy299 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy299/Solution.java
@@ -0,0 +1,28 @@
+package com.github.ryan.leetcode.algorithm.easy.easy299;
+
+public class Solution {
+
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] smap = new int[10]; // number 0 ~ 9
+        int[] gmap = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            char sch = secret.charAt(i);
+            char gch = guess.charAt(i);
+            if (sch == gch) {
+                bulls++;
+            } else {
+                smap[sch - '0']++;
+                gmap[gch - '0']++;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (smap[i] != 0 && gmap[i] != 0) {
+                cows += Math.min(smap[i], gmap[i]);
+            }
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
