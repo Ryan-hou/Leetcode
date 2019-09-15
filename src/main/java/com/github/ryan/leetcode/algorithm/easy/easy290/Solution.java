1ad9f76f8b81a88aa056cfb0b4facc7bef6b6d6d
ryan_hou <ryan.houyl@gmail.com>
2019-09-15 22:57:31 +0800

    easy290 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy290/Solution.java
@@ -0,0 +1,35 @@
+package com.github.ryan.leetcode.algorithm.easy.easy290;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        char[] charArray = pattern.toCharArray();
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> patternMap = new HashMap<>();
+        Set<String> valuesSet = new HashSet<>();
+
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            String word = words[i];
+            String mappedWord = patternMap.get(c);
+            if (mappedWord == null) {
+                patternMap.put(c, word);
+                valuesSet.add(word);
+            } else if (!mappedWord.equals(word)) {
+                return false;
+            }
+        }
+        return (patternMap.size() == valuesSet.size());
+    }
+
+}
