89f81a467253d1f1246ca867075a7a2d0cfb837d
ryan_hou <ryan.houyl@gmail.com>
2019-09-19 22:51:51 +0800

    easy594 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy594/Solution.java
@@ -0,0 +1,25 @@
+package com.github.ryan.leetcode.algorithm.easy.easy594;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int findLHS(int[] nums) {
+        if (nums == null || nums.length < 1) return 0;
+        // key -> num in nums, value -> num's count
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int res = 0;
+        for (int n : map.keySet()) {
+            if (map.containsKey(n + 1)) {
+                res = Math.max(res, map.get(n) + map.get(n + 1));
+            }
+        }
+        return res;
+    }
+
+}
