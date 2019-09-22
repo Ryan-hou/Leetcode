d9629de6a86ab4e585af8139e7b9f1ba3e8588e3
ryan_hou <ryan.houyl@gmail.com>
2019-09-22 19:55:31 +0800

    medium901 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium901/StockSpanner.java
@@ -0,0 +1,31 @@
+package com.github.ryan.leetcode.algorithm.medium.medium901;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StockSpanner {
+    private Deque<Integer> priceStack;
+    private Deque<Integer> spanStack;
+
+    public StockSpanner() {
+        priceStack = new ArrayDeque<>();
+        spanStack = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while (!priceStack.isEmpty() && priceStack.peek() <= price) {
+            priceStack.pop();
+            span += spanStack.pop();
+        }
+        priceStack.push(price);
+        spanStack.push(span);
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
