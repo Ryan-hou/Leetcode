b48645e82239e0e1b2817757f5c68406408010a4
ryan_hou <ryan.houyl@gmail.com>
2019-09-28 17:28:10 +0800

    medium524 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium524/Solution.java
@@ -0,0 +1,43 @@
+package com.github.ryan.leetcode.algorithm.medium.medium524;
+
+import java.util.List;
+
+public class Solution {
+
+    public String findLongestWord(String s, List<String> d) {
+        String ans = new String();
+        for (String word : d) {
+            if (word.length() < ans.length()
+                    || word.length() > s.length()
+                    || (word.length() == ans.length() && ans.compareTo(word) <= 0)) {
+                continue;
+            }
+            if (isMatched(word, s) && isBetterAns(word, ans)) {
+                ans = word;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isMatched(String word, String s) {
+        int startPos = 0;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = s.indexOf((int) word.charAt(i), startPos);
+            if (idx == -1) {
+                return false;
+            }
+            startPos = idx + 1;
+        }
+        return true;
+    }
+
+    private boolean isBetterAns(String word, String ans) {
+        if (word.length() > ans.length()
+                || (word.length() == ans.length() && word.compareTo(ans)  < 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
