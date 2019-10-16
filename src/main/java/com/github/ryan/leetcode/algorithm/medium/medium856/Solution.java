d7e26cba3a354508e5c2eb2f6844950bf766e307
ryan_hou <ryan.houyl@gmail.com>
2019-10-16 21:06:09 +0800

    medium856 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium856/Solution.java
@@ -0,0 +1,25 @@
+package com.github.ryan.leetcode.algorithm.medium.medium856;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+
+    // use stack:
+    // https://leetcode.com/problems/score-of-parentheses/solution/
+    public int scoreOfParentheses(String S) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0); // the score of the current frame
+        for (char ch : S.toCharArray()) {
+            if (ch == '(') {
+                stack.push(0);
+            } else {
+                int cur = stack.pop();
+                int prior = stack.pop();
+                stack.push(prior + Math.max(2 * cur, 1));
+            }
+        }
+        return stack.pop();
+    }
+
+}
