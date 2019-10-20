5ab3a5e9dad4d56fdf08be2a3c88923b21a3f1e9
ryan_hou <ryan.houyl@gmail.com>
2019-10-20 20:00:17 +0800

    hard895 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/hard/hard895/FreqStack.java
@@ -0,0 +1,44 @@
+package com.github.ryan.leetcode.algorithm.hard.hard895;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class FreqStack {
+
+    // max from x to the number of occurrences of x
+    private Map<Integer, Integer> freq;
+    // map from frequency to a stack of elements with that frequency
+    private Map<Integer, Stack<Integer>> group;
+    private int maxFreq;
+
+    public FreqStack() {
+        freq = new HashMap<>();
+        group = new HashMap<>();
+        maxFreq = 0;
+    }
+
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        if (f > maxFreq) {
+            maxFreq = f;
+        }
+        group.computeIfAbsent(f, z -> new Stack()).push(x);
+    }
+
+    public int pop() {
+        int x = group.get(maxFreq).pop();
+        freq.put(x, freq.get(x) - 1);
+        if (group.get(maxFreq).size() == 0) {
+            maxFreq--;
+        }
+        return x;
+    }
+}
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ */
