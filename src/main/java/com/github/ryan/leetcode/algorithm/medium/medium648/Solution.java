573afb0a8798028e47b7e05af6c3edf245c7e784
ryan_hou <ryan.houyl@gmail.com>
2019-10-24 23:21:28 +0800

    medium648 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium648/Solution.java
@@ -0,0 +1,47 @@
+package com.github.ryan.leetcode.algorithm.medium.medium648;
+
+import java.util.List;
+
+public class Solution {
+
+    private static class TrieNode {
+        TrieNode[] children;
+        String word;
+        TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+
+    // use Trie
+    public String replaceWords(List<String> dict, String sentence) {
+        TrieNode trie = new TrieNode();
+        for (String root : dict) {
+            TrieNode cur = trie;
+            for (char ch : root.toCharArray()) {
+                if (cur.children[ch - 'a'] == null) {
+                    cur.children[ch - 'a'] = new TrieNode();
+                }
+                cur = cur.children[ch - 'a'];
+            }
+            cur.word = root;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (String word : sentence.split("\\s+")) {
+            if (res.length() > 0) {
+                res.append(" ");
+            }
+
+            TrieNode cur = trie;
+            for (char ch : word.toCharArray()) {
+                if (cur.children[ch - 'a'] == null || cur.word != null) {
+                    break;
+                }
+                cur = cur.children[ch - 'a'];
+            }
+            res.append(cur.word != null ? cur.word : word);
+        }
+        return res.toString();
+    }
+
+}
