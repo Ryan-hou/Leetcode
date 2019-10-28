2d0d624633dab2a711cbedded9707c01ce74697f
ryan_hou <ryan.houyl@gmail.com>
2019-10-28 17:02:15 +0800

    medium826 is finished.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium826/Solution.java
@@ -0,0 +1,36 @@
+package com.github.ryan.leetcode.algorithm.medium.medium826;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    private static class Job {
+        int difficulty;
+        int profit;
+        Job(int difficulty, int profit) {
+            this.difficulty = difficulty;
+            this.profit = profit;
+        }
+    }
+
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int n = difficulty.length;
+        Job[] jobs = new Job[n];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new Job(difficulty[i], profit[i]);
+        }
+        Arrays.sort(jobs, (a, b) -> a.difficulty - b.difficulty);
+        Arrays.sort(worker);
+        // no need to reset i and best
+        int res = 0, i = 0, best = 0;
+        for (int skill : worker) {
+            while (i < n && skill >= jobs[i].difficulty) {
+                best = Math.max(best, jobs[i].profit);
+                i++;
+            }
+            res += best;
+        }
+        return res;
+    }
+
+}
