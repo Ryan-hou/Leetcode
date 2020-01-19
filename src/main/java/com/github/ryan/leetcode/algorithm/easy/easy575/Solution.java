73dd829e9fd28f50b74922a319db07f5bd22c515
yanlin.hou <ryan.houyl@gmail.com>
2020-01-19 15:06:21 +0800

    easy575 is solved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy575/Solution.java
@@ -0,0 +1,22 @@
+package com.github.ryan.leetcode.algorithm.easy.easy575;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public int distributeCandies(int[] candies) {
+        if (candies == null || candies.length < 1) {
+            return 0;
+        }
+
+        int num = candies.length / 2;
+        Set<Integer> kindSet = new HashSet<>();
+        for (int kind : candies) {
+            kindSet.add(kind);
+        }
+        int kindNum = kindSet.size();
+        return kindNum >= num ? num : kindNum;
+    }
+
+}
