026a92a398019b87c86c5601911821d0c36767c3
yanlin.hou <ryan.houyl@gmail.com>
2020-04-02 14:43:12 +0800

    easy453 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/easy/easy453/Solution.java
@@ -0,0 +1,20 @@
+package com.github.ryan.leetcode.algorithm.easy.easy453;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int min = nums[0], max = nums[len - 1];
+        int moves = max - min; // try to make max is the equal element
+        for (int i = 1; i < len - 1; i++) {
+            if (nums[i] != min) {
+                moves += (nums[i] - min);
+            }
+        }
+        return moves;
+    }
+
+}
