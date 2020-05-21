575ffebdbb2f8e1b44496eeba7c4ebe82c749305
yanlin.hou <ryan.houyl@gmail.com>
2020-05-21 16:23:25 +0800

    medium1079 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium1079/Solution.java
@@ -0,0 +1,27 @@
+package com.github.ryan.leetcode.algorithm.medium.medium1079;
+
+public class Solution {
+
+    private int count = 0;
+
+    public int numTilePossibilities(String tiles) {
+        int[] dict = new int[26];
+        for (char ch : tiles.toCharArray()) {
+            dict[ch - 'A']++;
+        }
+        calc(dict);
+        return count;
+    }
+
+    private void calc(int[] dict) {
+        for (int i = 0; i < dict.length; i++) {
+            if (dict[i] != 0) {
+                count++;
+                dict[i]--;
+                calc(dict);
+                dict[i]++;
+            }
+        }
+        // 递归出口
+    }
+}
