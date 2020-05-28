f60451971faf2d5f027335de57fa2de6f39e12a1
yanlin.hou <ryan.houyl@gmail.com>
2020-05-28 10:06:14 +0800

    medium973 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium973/Solution.java
@@ -0,0 +1,29 @@
+package com.github.ryan.leetcode.algorithm.medium.medium973;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+
+    // Heap
+    public int[][] kClosest(int[][] points, int K) {
+        // max heap
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>((a1, a2)
+                        -> (a2[0] * a2[0] + a2[1] *a2[1]) - (a1[0] * a1[0] + a1[1] * a1[1]));
+
+        for (int i = 0; i < points.length; i++) {
+            pq.offer(points[i]);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
+
+        int[][] res = new int[K][2];
+        for (int i = K - 1; i >= 0; i--) {
+            int[] ele = pq.poll();
+            res[i][0] = ele[0];
+            res[i][1] = ele[1];
+        }
+        return res;
+    }
+}
