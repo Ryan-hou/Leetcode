a8e669960c1accf9641288af7fa33952d5c4bc83
yanlin.hou <ryan.houyl@gmail.com>
2020-06-11 10:14:28 +0800

    medium366 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium366/Solution.java
@@ -0,0 +1,30 @@
+package com.github.ryan.leetcode.algorithm.medium.medium366;
+
+import com.github.ryan.personal.algorithm.component.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    // return root's level, leaf is level 0
+    private int dfs(TreeNode root, List<List<Integer>> res) {
+        if (root == null) {
+            return -1; // 递归出口
+        }
+
+        int level = Math.max(dfs(root.left, res), dfs(root.right, res)) + 1;
+        if (res.size() <= level) {
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        return level;
+    }
+
+}
