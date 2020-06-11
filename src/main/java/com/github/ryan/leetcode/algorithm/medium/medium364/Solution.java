a1cb31f3cee9c960df61d320254d335f7e3b42ff
yanlin.hou <ryan.houyl@gmail.com>
2020-06-11 09:52:21 +0800

    medium364 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium364/Solution.java
@@ -0,0 +1,39 @@
+package com.github.ryan.leetcode.algorithm.medium.medium364;
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.github.ryan.leetcode.algorithm.easy.easy339.Solution.*;
+
+public class Solution {
+
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        if (nestedList == null || nestedList.size() < 1) return 0;
+
+        List<Integer> levelSum = new ArrayList<>();
+        depthSum(nestedList, 0, levelSum);
+        int depth = levelSum.size();
+        int res = 0;
+        for (int num : levelSum) {
+            res += num * depth;
+            depth--;
+        }
+        return res;
+    }
+
+    // level traversal
+    private void depthSum(List<NestedInteger> nestedList, int level, List<Integer> levelSum) {
+        if (level + 1 > levelSum.size()) {
+            levelSum.add(0);
+        }
+        int sum = 0;
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) {
+                sum += ni.getInteger();
+            } else {
+                depthSum(ni.getList(), level + 1, levelSum);
+            }
+        }
+        levelSum.set(level, levelSum.get(level) + sum);
+    }
+
+}
