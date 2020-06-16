2bb4847739f67b694eb6cf07f395ca9101d4c51d
yanlin.hou <ryan.houyl@gmail.com>
2020-06-16 11:37:46 +0800

    medium362 is resolved.

---
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium362/HitCounter.java
@@ -0,0 +1,45 @@
+package com.github.ryan.leetcode.algorithm.medium.medium362;
+
+import java.util.TreeMap;
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
+public class HitCounter {
+
+    // key -> timestamp, value -> hits at this timestamp
+    private TreeMap<Integer, Integer> map;
+    private static final int TIME_LIMIT = 300; // 300 seconds
+
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        map = new TreeMap<>();
+    }
+
+    /** Record a hit.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        map.put(timestamp, map.getOrDefault(timestamp, 0) + 1);
+        // clean up before getHits called
+        while (timestamp - map.firstKey() >= TIME_LIMIT) {
+            map.pollFirstEntry();
+        }
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while (!map.isEmpty() && timestamp - map.firstKey() >= TIME_LIMIT) {
+            map.pollFirstEntry();
+        }
+        int count = 0;
+        for (int hit : map.values()) {
+            count += hit;
+        }
+        return count;
+    }
+
+}
