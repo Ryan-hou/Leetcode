435940540305b2e7a5381dad6be7cf065a3fde2e
yanlin.hou <ryan.houyl@gmail.com>
2020-07-07 15:58:40 +0800

    medium785 is resolved.

--- src/main/java/com/github/ryan/leetcode/algorithm/medium/medium314/Solution.java
+++ src/main/java/com/github/ryan/leetcode/algorithm/medium/medium314/Solution.java
@@ -15,17 +15,17 @@ public class Solution {
         Map<Integer, List<Integer>> colTable = new HashMap<>();
         // Pair: key -> TreeNode, value -> col of this node
         Queue<Pair<TreeNode, Integer>> q = new ArrayDeque<>();
-        q.offer(new Pair(root, 0));
+        q.offer(new Pair<>(root, 0));
         while (!q.isEmpty()) {
             Pair<TreeNode, Integer> pair = q.poll();
             TreeNode cur = pair.getKey();
             int col = pair.getValue();
             colTable.computeIfAbsent(col, x -> new ArrayList<>()).add(cur.val);
             if (cur.left != null) {
-                q.offer(new Pair(cur.left, col - 1));
+                q.offer(new Pair<>(cur.left, col - 1));
             }
             if (cur.right != null) {
-                q.offer(new Pair(cur.right, col + 1));
+                q.offer(new Pair<>(cur.right, col + 1));
             }
         }
 
