c1cfa65e90ff2038d2560d9c3652ede2863fc4c3
yanlin.hou <ryan.houyl@gmail.com>
2020-09-21 14:58:03 +0800

    Unweighted graph completed.

---
+++ src/main/java/com/github/ryan/personal/data_structure/graph/base/Graph.java
@@ -0,0 +1,39 @@
+package com.github.ryan.personal.data_structure.graph.base;
+
+public interface Graph {
+
+    /**
+     * 图的顶点数
+     * @return
+     */
+    int V();
+
+    /**
+     * 图的边数
+     * @return
+     */
+    int E();
+
+    /**
+     * 顶点 v,w 是否存在边
+     * @param v
+     * @param w
+     * @return
+     */
+    boolean hasEdge(int v, int w);
+
+    /**
+     * 顶点 v 的相邻顶点
+     * @param v
+     * @return
+     */
+    Iterable<Integer> adj(int v);
+
+    /**
+     * 顶点 v 的度
+     * @param v
+     * @return
+     */
+    int degree(int v);
+
+}
---
+++ src/main/java/com/github/ryan/personal/data_structure/graph/base/UnweightedGraph.java
@@ -0,0 +1,119 @@
+package com.github.ryan.personal.data_structure.graph.base;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/**
+ * 无权图
+ * 简单图,不含自环边和平行边
+ */
+@Slf4j
+public class UnweightedGraph implements Graph {
+
+    private int V;
+    private int E;
+    /**
+     * 顶点对应的相邻顶点集合
+     * 也可以采用 HashSet, 这里使用红黑树的顺序性来方便测试
+     */
+    private TreeSet<Integer>[] adj;
+
+    public UnweightedGraph(String filename) {
+        String parentPath = UnweightedGraph.class.getResource("/").getPath();
+        String subPath = "graph" + File.separator + filename;
+        File file = new File(parentPath + subPath);
+
+        try (Scanner scanner = new Scanner(file)) {
+            V = scanner.nextInt();
+            if (V < 0) {
+                throw new IllegalArgumentException("V must be non-negative");
+            }
+            E = scanner.nextInt();
+            if (E < 0) {
+                throw new IllegalArgumentException("E must be non-negative");
+            }
+
+            adj = new TreeSet[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new TreeSet<>();
+            }
+
+            for (int i = 0; i < E; i++) {
+                int a = scanner.nextInt();
+                validateVertex(a);
+                int b = scanner.nextInt();
+                validateVertex(b);
+
+                if (a == b) {
+                    throw new IllegalArgumentException("Self Loop is Detected!");
+                }
+                if (adj[a].contains(b)) {
+                    throw new IllegalArgumentException("Parallel Edges are Detected!");
+                }
+
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+
+        } catch (Exception e) {
+            log.error("UnweightedGraph construct exception!", e);
+        }
+    }
+
+    @Override
+    public int V() {
+        return V;
+    }
+
+    @Override
+    public int E() {
+        return E;
+    }
+
+    @Override
+    public boolean hasEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v].contains(w);
+    }
+
+    @Override
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    @Override
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("V = %d, E = %d\n", V, E));
+        for (int v = 0; v < V; v++) {
+            sb.append(String.format("%d : ", v));
+            for (int w : adj[v]) {
+                sb.append(String.format("%d ", w));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is invalid");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Graph g = new UnweightedGraph("g.txt");
+        System.out.println(g);
+    }
+}
