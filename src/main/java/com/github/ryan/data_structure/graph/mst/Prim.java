57b7e1452c010292657fa18ef911cfeb7989620a
yanlin.hou <ryan.houyl@gmail.com>
2020-09-23 18:24:06 +0800

    Prim 算法实现

---
+++ src/main/java/com/github/ryan/data_structure/graph/mst/Prim.java
@@ -0,0 +1,60 @@
+package com.github.ryan.data_structure.graph.mst;
+
+import com.github.ryan.data_structure.graph.base.CC;
+import com.github.ryan.data_structure.graph.base.WeightedEdge;
+import com.github.ryan.data_structure.graph.base.WeightedGraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prim {
+
+    private WeightedGraph G;
+    private List<WeightedEdge> mst;
+
+    public Prim(WeightedGraph g) {
+        G = g;
+        mst = new ArrayList<>();
+
+        CC cc = new CC(g);
+        if (cc.count() > 1) {
+            return;
+        }
+
+        // Prim
+        // 正向使用切分定理，每次寻找最小的横切边
+        boolean[] visited = new boolean[G.V()];
+        visited[0] = true;
+        // 时间复杂度 O((v - 1) * (V + E)) -> O(VE)
+        // 获取 V - 1 条横切边构成 mst
+        for (int i = 1; i < G.V(); i++) {
+
+            WeightedEdge minEdge = new WeightedEdge(-1, - 1, Integer.MAX_VALUE);
+            // 遍历所有横切边，选取最小值
+            for (int v = 0; v < G.V(); v++) {
+                if (visited[v]) {
+                    for (int w : G.adj(v)) {
+                        if (!visited[w] && G.getWeight(v, w) < minEdge.getWeight()) {
+                            minEdge = new WeightedEdge(v, w, G.getWeight(v, w));
+                        }
+                    }
+                }
+            }
+
+            mst.add(minEdge);
+            visited[minEdge.getV()] = true;
+            visited[minEdge.getW()] = true;
+        }
+
+    }
+
+    public List<WeightedEdge> mst() {
+        return mst;
+    }
+
+    public static void main(String[] args) {
+        WeightedGraph wg = new WeightedGraph("mst.txt");
+        Prim prim = new Prim(wg);
+        System.out.println("Minimal spanning is: " + prim.mst);
+    }
+}
---
+++ src/main/java/com/github/ryan/data_structure/graph/mst/Prim2.java
@@ -0,0 +1,67 @@
+package com.github.ryan.data_structure.graph.mst;
+
+import com.github.ryan.data_structure.graph.base.CC;
+import com.github.ryan.data_structure.graph.base.WeightedEdge;
+import com.github.ryan.data_structure.graph.base.WeightedGraph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 使用优先队列优化 Prim 算法
+ * 时间复杂度 O(ElogE)
+ */
+public class Prim2 {
+
+    private WeightedGraph G;
+    private List<WeightedEdge> mst;
+
+    public Prim2(WeightedGraph G) {
+        this.G = G;
+        mst = new ArrayList<>();
+
+        CC cc = new CC(G);
+        if (cc.count() > 1) {
+            return;
+        }
+
+        // Prim
+        boolean[] visited = new boolean[G.V()]; // 用于区分切分
+        // min heap
+        Queue<WeightedEdge> pq = new PriorityQueue<>();
+        visited[0] = true;
+        for (int w : G.adj(0)) {
+            pq.offer(new WeightedEdge(0, w, G.getWeight(0, w)));
+        }
+
+        while (!pq.isEmpty()) {
+            // 切分变化后，存在无效的横切边
+            WeightedEdge edge = pq.poll();
+            if (visited[edge.getV()] && visited[edge.getW()]) {
+                continue;
+            }
+
+            mst.add(edge);
+            int newv = visited[edge.getV()] ? edge.getW() : edge.getV();
+            visited[newv] = true;
+            // 新增横切边
+            for (int w : G.adj(newv)) {
+                if (!visited[w]) {
+                    pq.offer(new WeightedEdge(newv, w, G.getWeight(newv, w)));
+                }
+            }
+        }
+    }
+
+    public List<WeightedEdge> mst() {
+        return mst;
+    }
+
+    public static void main(String[] args) {
+        WeightedGraph wg = new WeightedGraph("mst.txt");
+        Prim2 prim2 = new Prim2(wg);
+        System.out.println("Minimal spanning tree is: " + prim2.mst());
+    }
+}
